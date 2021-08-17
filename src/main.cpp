faed3476f5300ab2b9b0b6f6c94202de67c7d626
Jari Braeckman <braeckmanjari@gmail.com>
2021-08-17 15:00:25 +0200

    Update main.cpp
    Zoom in with arrow keys, stop with esc key

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
 #include "Mandelbrot.h"
+#include <conio.h>
+const int KEY_UP = 72;
+const int KEY_DOWN = 80;
+const int KEY_LEFT = 75;
+const int KEY_RIGHT = 77;
+const int KEY_ARROW = 224;
+const int KEY_ESC = 27;
 
 int main() {
     cout << "Start with real axis [-2, 2] and imaginary axis [-2, 2]:" << endl;
-    MandelbrotSet m = MandelbrotSet((double) -2.0, (double) 2.0, (double) -2.0, (double) 2.0, (int) 200, (int) 50);
+    MandelbrotSet m = MandelbrotSet((double) -1.0, (double) 1.0, (double) -1.0, (double) 1.0, (int) 100, (int) 50);
     m.showGrid();
     bool stop = false;
     char ans;
-    double zoom;
-    double real_min;
-    double real_max;
-    double imag_min;
-    double imag_max;
-    double nx;
-    double ny;
+    double real_min = -1.0;
+    double real_max = 1.0;
+    double imag_min = -1.0;
+    double imag_max = 1.0;
+    double nx = 100;
+    double ny = 50;
     while (!stop) {
-        cout << "Continue? (y/n): " << endl;
-        cin >> ans;
-        if (ans == 'y'){
-            cout << "Lower limit for real axis: " << endl;
-            cin >> real_min;
-            cout << "Upper limit for real axis: " << endl;
-            cin >> real_max;
-            cout << "Lower limit for imaginary axis: " << endl;
-            cin >> imag_min;
-            cout << "Upper limit for imaginary axis: " << endl;
-            cin >> imag_max;
-            nx = m.nx;
-            ny = m.ny;
+        cout << "Zoom in with arrow keys" << endl;
+        int ch = _getch();
+        if (ch == KEY_ARROW){
+            int ch = _getch();
+            if (ch == KEY_UP) {
+                imag_min = (imag_max + imag_min) / 2.0;
+            }
+            if (ch == KEY_DOWN) {
+                imag_max = (imag_max + imag_min) / 2.0;
+            }
+            if (ch == KEY_RIGHT) {
+                real_min = (real_max + real_min) / 2.0;
+            }
+            if (ch == KEY_LEFT) {
+                real_max = (real_max + real_min) / 2.0;
+            }
             MandelbrotSet m = MandelbrotSet(real_min, real_max, imag_min, imag_max, nx, ny);
-            cout << m.real_min << endl;
             m.showGrid();
         }
         else {
-            if (ans == 'n'){
+            if (ch == KEY_ESC){
                 stop = true;
             }
             else {
                 cout << "Invalid answer" << endl;
             }
         }
-
     }
     return 0;
 }; 
